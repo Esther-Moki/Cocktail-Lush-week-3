d4179b51099c52e94d34859ad74cda2d93089bca
Esther-Moki <esther.moki@student.moringaschool.com>
2021-10-24 18:17:22 +0300

    added the click listener to the create account button

--- app/src/main/java/com/moringaschool/cocktaillush/ui/LoginActivity.java
+++ app/src/main/java/com/moringaschool/cocktaillush/ui/LoginActivity.java
@@ -2,15 +2,36 @@ package com.moringaschool.cocktaillush.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.moringaschool.cocktaillush.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.registerTextView)
+    TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView){
+            Intent intent = new Intent(LoginActivity.this,CreateAccountActivity.class);
+            startActivity(intent);
+            finish();// this destroys the LoginActivity as we depart for the CreateAccountActivity
+        }
+
     }
 }
